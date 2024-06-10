9f217cdacccfc31c76b5ebab276f0255b60f381a
Noushin <nou.babakhani@gmail.com>
2024-06-10 13:19:57 +0100

    Added assertion to class Form

--- start/test/Form.java
+++ start/test/Form.java
@@ -1,6 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 public class Form {
     WebDriver driver;
@@ -28,6 +33,14 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
 
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until((ExpectedConditions.visibilityOfElementLocated(By.className("alert"))));
+
+        String alertText = alert.getText();
+
+        assertEquals("The form was successfully submitted!", alertText);
+
         driver.quit();
     }
 }
